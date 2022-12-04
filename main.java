d3d2071b4b1cd3a940639f4236c574c8f227636e
Bruno Rebic <119545220+brunorebic@users.noreply.github.com>
2022-12-04 22:57:45 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("Learning git from AmigosCode course!");
-        System.out.println("Having fun!");
+        System.out.println("This is good!");
     }
 }
 
